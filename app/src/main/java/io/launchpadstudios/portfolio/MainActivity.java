c1fb0daac9486e8f3136ad011f8fe8a3ab3f0f4d
Olivier Melcher <olivier.melcher@gmail.com>
2016-09-09 11:06:26 -0500

    add toasts

--- app/src/main/java/io/launchpadstudios/portfolio/MainActivity.java
+++ app/src/main/java/io/launchpadstudios/portfolio/MainActivity.java
@@ -1,13 +1,31 @@
 package io.launchpadstudios.portfolio;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAppButton(View view){
+        // get text of the button
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+
+        // create a toast
+        Context context = getApplicationContext();
+        CharSequence text = String.format("This button will launch the %s app!", buttonText);
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
